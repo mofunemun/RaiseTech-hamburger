8757917208395ca343d8cb02f05ad3c83a8bea25
mofunemmn <sinkamofunemu@gmail.com>
2020-07-11 21:57:32 +0900

    add new changes

--- js/hamburgerMenu.js
+++ js/hamburgerMenu.js
@@ -1,9 +1,5 @@
-jQuery( function( $ ) {
-    $( ".is-hamburger" ).click( function() {
-        $( this ).toggleClass( "is-open" );
-        $( ".p-gmenu" ).toggleClass( "is-open" );         
-        $( "body" ).toggleClass( "is-open" );    
-    });
+$('.header__menu').click(function(){
+    $(aside).toggleClass('is-open');
+    $('.aside__nav__button').toggleClass('is-open');
+    $('.header__menu').toggleClass('is-open');
 });
-
-console.log("sake");
